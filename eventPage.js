bb054464d7416255e2bbb604856b7058538469cf
yunhai <wangyh@yeah.net>
2018-02-19 00:43:13 +0800

    This time is true: improve user interface & button to clear individual task.

--- eventPage.js
+++ eventPage.js
@@ -21,17 +21,48 @@ function printItems(){
 	document.getElementById("data").innerHTML = "";
 	for(var key in window.object){
 		var content = document.getElementById("data");
-	 	var text = document.createTextNode(object[key]);
+	 	
+    // Create text element
+    // Create paragraph element
+    // Create button element
+    // Create div element (to hold both text & button)
+    // Create div_text element
+    // Create div_button element 
+    
+    var text = document.createTextNode(object[key]);
+    var para = document.createElement("p");
     var button = document.createElement("button");
-    button. innerHTML = "finish";
+    var div_entry = document.createElement("div");
+    var div_text = document.createElement("div");
+    var div_button = document.createElement("div");
+
+    // Add javscript features to button element.
+    // button. innerHTML = "finish";
     button.id = key;
-    button.class = "finish";
-	 	var para = document.createElement("p");
-	 	para.appendChild(text);
-	 	content.appendChild(para);	
-    content.appendChild(button);
+    button.value = key;
+    button.addEventListener('click', function(event) {clear_a_task(this.value)}, false);
+    button.className = "class_clear_task_button";
+    // Add javscript features to div_entry element.
+    div_text.id = "id_text";
+    div_button.id = "id_button";
+    div_entry.id = "id_entry";
+    // Arrange the sequence of all the elements.
+    // Put textNode into paragraph element.
+    // Put para and button into div_entry.
+    // Lastly, put entry_div into "data" area.
+    para.appendChild(text);
+    div_text.appendChild(para);
+    div_button.appendChild(button);
+    
+    div_entry.appendChild(div_text);
+    div_entry.appendChild(div_button);
+    content.appendChild(div_entry);
 	}
-	
+ }
+
+ function clear_a_task(value){
+  delete window.object[value];
+  printItems();
  }
  
 function add() {
@@ -60,10 +91,6 @@ document.getElementById("add").onclick = function(){
  }
 
 
-document.getElementByClassName("finish").onclick = function(){
-    
- }
-
 document.getElementById("clear").onclick = function(){
     clear();
  }
@@ -90,9 +117,6 @@ chrome.storage.onChanged.addListener(function(changes, namespace) {
   });
   }
 
-
-
-// printItems();
 
 
 
